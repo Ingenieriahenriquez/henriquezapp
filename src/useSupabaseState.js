fbd8f6ea8641319eedaa13aa010d2da24b476181
Ingenieriahenriquez <ingenieriaytecnologiahenriquez@gmail.com>
2026-09-22 06:50:10 -0400

    Add facturaSuplidor to CAMEL_TO_SNAKE mapping

--- src/useSupabaseState.js
+++ src/useSupabaseState.js
@@ -20,6 +20,7 @@ const CAMEL_TO_SNAKE = {
   fechaHora: "fecha_hora",
   facturaId: "factura_id",
   creadoEn: "creado_en",
+  facturaSuplidor: "factura_suplidor",
 };
 const SNAKE_TO_CAMEL = Object.fromEntries(
   Object.entries(CAMEL_TO_SNAKE).map(([camel, snake]) => [snake, camel])
